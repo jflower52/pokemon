61090f9c8f508f6220d74c7d2130aee9aca25d34
jflower <jflower0502@gmail.com>
2025-04-24 10:12:43 +0900

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from "react";
+import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
